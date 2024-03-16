ac644366ea4e7da00ebc8d8ad91c44906f78cab5
Shawn Reece <14791537+sreece52@users.noreply.github.com>
2024-03-16 15:44:55 -0400

    Add files via upload

---
+++ commands/utility/war-status.js
@@ -0,0 +1,71 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('war-status')
+		.setDescription('Receive latest galactic war status'),
+	async execute(interaction) {
+		let warStatus = await getWarStatus();
+
+		let warStatusJson = await warStatus.json();
+
+		let campaigns = warStatusJson.campaigns;
+
+		let activeCompaignPlanetIndexes = campaigns.map((campaign) => { return campaign.planet.index });
+
+		let planets = getPlanets(activeCompaignPlanetIndexes, warStatusJson.planet_status);
+
+		let message = "# <TRANSMISSION RECIEVED>\n"
+
+		message += "# __Active Campaigns__\n";
+
+		message += createPlanetMessage(planets);
+
+		message += "# __Major Orders__\n";
+
+		console.log(warStatusJson.global_events.forEach((orders) => {
+			message += orders.message.en + "\n"
+		}));
+
+		message += "# <END OF TRANSMISSION>\n"
+
+		await interaction.reply(message);
+	},
+};
+
+async function getWarStatus() {
+	return fetch('https://helldivers-2.fly.dev/api/801/status');
+}
+
+function getPlanets(indexes, planetStatus) {
+
+	let planets = [];
+
+	indexes.forEach((index) => {
+		planets.push(planetStatus.filter((planetStatus) => planetStatus.planet.index === index)[0]); 
+	})
+
+	console.log(planets)
+
+	return planets;
+}
+
+function createPlanetMessage(planets) {
+	let message = "";
+	planets.forEach((p) => {
+		message += `## __${p.planet.name}__\n`;
+		message += `> Campaign Type: ${getCampaignType(p.owner)}\n`
+		message += `> Status: ${parseFloat(p.liberation).toFixed(2)} %\n`;
+		message += `> Hell Divers Deployed: ${p.players}\n`;
+	});
+
+	return message;
+}
+
+function getCampaignType(owner) {
+	if (owner == "Humans") {
+		return "Defend";
+	} 
+
+	return owner === "Humans" ? "Defend" : "Liberation";
+}
